4f3d66a4dc422965db2d5f64294f220cdc4febaa
Jaden <43506164+soravoid@users.noreply.github.com>
2024-08-29 02:30:32 -0500

    Add Hyprland and Anyrun cachix

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,17 @@
 
 { config, lib, pkgs, ... }:
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://anyrun.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+    ];
+  };
 
   # Use the GRUB 2 boot loader.
   boot.loader.systemd-boot.enable = true;
